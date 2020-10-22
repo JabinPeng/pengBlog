db7d0b167207d407f5b2c7f7e96becfaa1d40dc4
JabinPeng <690996726@qq.com>
2020-10-22 16:49:24 +0800

    :fire:

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -95,31 +95,5 @@ module.exports = {
   },
   "markdown": {
     "lineNumbers": true
-  },
-  "plugins": [
-    'vuepress-plugin-helper-live2d', {
-      // 是否开启控制台日志打印(default: false)
-      log: false,
-      live2d: {
-        // 是否启用(关闭请设置为false)(default: true)
-        enable: true,
-        // 模型名称(default: hibiki)>>>取值请参考：
-        // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-        model: 'miku',
-        display: {
-          position: "left", // 显示位置：left/right(default: 'right')
-          width: 135, // 模型的长度(default: 135)
-          height: 300, // 模型的高度(default: 300)
-          hOffset: 65, //  水平偏移(default: 65)
-          vOffset: 0, //  垂直偏移(default: 0)
-        },
-        mobile: {
-          show: false // 是否在移动设备上显示(default: false)
-        },
-        react: {
-          opacity: 1 // 模型透明度(default: 0.8)
-        }
-      }
-    }
-  ],
+  }
 }
