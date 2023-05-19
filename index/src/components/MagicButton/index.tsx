06e7ada97c56c5a70d8d4930455506953fc08a47
Jabin Li <690996726@qq.com>
2023-05-19 17:34:35 +0800

    🚀 施工

--- index/src/components/MagicButton/index.tsx
+++ index/src/components/MagicButton/index.tsx
@@ -1,33 +1,30 @@
-import React, { useRef } from 'react'
+import React, { useRef, ReactNode } from 'react'
 import styles from "./styles.module.scss";
 
 
 type Props = {
-  children: String;
+  children: ReactNode;
 }
 
 const MagicButton = (props:Props) => {
 
   const { children } = props;
 
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const buttonRef = useRef<HTMLDivElement | null>(null);
 
   const handleMove = (event:any) => {
     const target: EventTarget | null | any  = event.target;
     const rect= target?.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    console.log(x, y);
     buttonRef?.current?.style.setProperty("--x", `${x}px`);
     buttonRef?.current?.style.setProperty("--y", `${y}px`);
-    // buttonRef?.current?.style.setProperty("--height", `${rect.height}px`);
-    // buttonRef?.current?.style.setProperty("--width", `${rect.width}px`);
   };
 
   return (
     <div className={styles.magicButton}> 
-      <div className={styles.inner}>
-        <button type="button" onMouseMove={handleMove} ref={buttonRef}>{children}</button>
+      <div className={styles.inner} ref={buttonRef}>
+        <button type="button" onMouseMove={handleMove}>{children}</button>
         <div className={styles.blob}></div>
       </div>
     </div>
--- index/src/sections/Project/index.tsx
+++ index/src/sections/Project/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './index.module.scss';
 import rocket from "@/assets/images/cartoon-rocket.webp";
-import MButton from "@/components/MagicButton"
+import { Col, Row, Space } from "antd"
 
 type Props = {}
 
 const Project = (props: Props) => {
+  const projectList = [
+    {
+      img: 'https://andycao.me/static/images/project/cyvertex-cover.jpg',
+      alt: '',
+      title: 'Cyber Vertex News Portal',
+      desc: 'The Cyber Vertex project integrates cybersecurity and digital forensics news data and learning resources. Provide a one-stop network security-related content provision platform. It also provides some social functions, to help learners to communicate and learn in a technical community. It may promote the development of cybersecurity and digital forensics field.',
+      technical: 'web & mobile',
+      href: ''
+    }
+  ]
+
+
   return (
     <div className={styles.project} id="project">
       <div className={styles.workspace}>
@@ -15,8 +27,24 @@ const Project = (props: Props) => {
         <h2 className={styles.headline}>个人项目</h2>
         <p className={styles.desc}>一些好玩的项目</p>
       </div>
-
-      <MButton >了解更多</MButton>
+      <Row className={styles.projectList}>
+        { projectList.map((item, i) => (
+          <Col span={24} md={12} className={styles.projectItem} key={item.title + i}>
+           <a href={item.href} target="_blank">
+              <div className={styles.projectImg}>
+                <img src={item.img} alt={item.alt} />
+              </div>
+              <div className={styles.projectInfo}>
+                <Space direction="vertical">
+                  <h2 className={styles.title}>{item.title}</h2>
+                  <span>{item.technical}</span>
+                  <p className={styles.desc}>{item.desc}</p>
+                </Space>
+              </div>
+           </a>
+        </Col>
+        )) }
+      </Row>
     </div>
   )
 }
